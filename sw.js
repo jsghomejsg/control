efdb67120d43d348d59b2957e9e434bd4daca05a
jsghomejsg <jsghomejsg@gmail.com>
2026-03-18 12:03:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'control-piscinas-v2';
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activar y limpiar cachés antiguas
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia de carga: Primero Cache, luego Red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
